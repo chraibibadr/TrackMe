60322c4afa01c470f0a1d0bb77459c0c4358ae1c
BADER <badr.chraibibadr@gmail.com>
2022-04-10 22:45:50 +0000

    First Commit

---
+++ app/src/main/java/ma/emsi/tpmap/MapsActivity.java
@@ -0,0 +1,240 @@
+package ma.emsi.tpmap;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import ma.emsi.tpmap.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    String TAG = "MapsActivity";
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
+    String showUrl = "https://10.0.2.2/localisation/loadPositions.php";
+    String insertUrl = "https://10.0.2.2/localisation/createPosition.php";
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleSSLHandshake();
+
+        //Localisation Permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Permissions.Request_FINE_LOCATION(this, 20);
+            return;
+        }
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 150, new
+                LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        altitude = location.getAltitude();
+                        accuracy = location.getAccuracy();
+                        @SuppressLint("StringFormatMatches") String msg = String.format(
+                                getResources().getString(R.string.new_location), latitude, longitude, altitude, accuracy);
+                        addPosition(latitude, longitude);
+                        getPositions();
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                        String newStatus = "";
+                        switch (status) {
+                            case LocationProvider.OUT_OF_SERVICE:
+                                newStatus = "OUT_OF_SERVICE";
+                                break;
+                            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                                newStatus = "TEMPORARILY_UNAVAILABLE";
+                                break;
+                            case LocationProvider.AVAILABLE:
+                                newStatus = "AVAILABLE";
+                                break;
+                        }
+                        String msg = String.format(getResources().getString(R.string.provider_new_status),
+                                provider, newStatus);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_enabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        String msg = String.format(getResources().getString(R.string.provider_disabled),
+                                provider);
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        getPositions();
+    }
+
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void getPositions() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(showUrl, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+                try {
+                    JSONArray positions = response.getJSONArray("positions");
+                    for (int i = 0; i < positions.length(); i++) {
+                        JSONObject position = positions.getJSONObject(i);
+
+                        //mMap.clear();
+                        LatLng pos = new LatLng(position.getDouble("latitude"), position.getDouble("longitude"));
+                        mMap.addMarker(new MarkerOptions().position(pos).title("Position" + " " + position.getInt("id")));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, error.toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    void addPosition(final double lat, final double lon) {
+        StringRequest request = new StringRequest(Request.Method.POST,
+                insertUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("MainActivity", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MainActivity", error.toString());
+            }
+        }) {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                TelephonyManager telephonyManager =
+                        (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                HashMap<String, String> params = new HashMap<String, String>();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                params.put("latitude", lat + "");
+                params.put("longitude", lon + "");
+                params.put("date", sdf.format(new Date()) + "");
+                //params.put("imei", telephonyManager.getImei());
+                params.put("imei", "not supported");
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+}
